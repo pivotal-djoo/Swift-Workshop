efd3984f095af5a413b0985d9652dfb012eb1f4e
DavidJ <djoo@pivotal.io>
2016-10-27 02:48:25 -0400

    Project setup - Added an iOS Xcode project - Added Cocoapods - Added Quick and Nimble test frameworks

---
+++ SwiftSample/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  SwiftSample
+//
+//  Created by NinjaSupreme on 2016-10-25.
+//  Copyright © 2016 Pivotal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
