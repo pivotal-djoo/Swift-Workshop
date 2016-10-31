be1220822ce0024ff5cdc6453e72501378984d51
DavidJ <djoo@pivotal.io>
2016-10-30 23:00:22 -0400

    Project setup using Xcode 8.1 - Added Cocoapods - Added Quick and Nimble test frameworks

---
+++ SwiftSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftSample
+//
+//  Created by NinjaSupreme on 2016-10-30.
+//  Copyright © 2016 Pivotal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
