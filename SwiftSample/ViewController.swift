7739da71545a46a184828d3095b87c0617256905
DavidJ <djoo@pivotal.io>
2016-10-30 23:03:02 -0400

    Added a greeting label and a test

--- SwiftSample/ViewController.swift
+++ SwiftSample/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        label.text = "Hello World!"
     }
 
     override func didReceiveMemoryWarning() {
