c1df1a224bc6dd567956678d39ca1ac921e281fa
DavidJ <djoo@pivotal.io>
2016-11-11 02:34:19 -0500

    Part 2 - Basic Networking Starting Point

--- SwiftSample/ViewController.swift
+++ SwiftSample/ViewController.swift
@@ -30,67 +30,12 @@ class ViewController: UIViewController {
         imageLoadingIndicator.hidesWhenStopped = true
         beveragesLoadingIndicator.hidesWhenStopped = true
         
-        // fetch image
+        // Start here
         
-        imageLoadingIndicator.startAnimating()
-        
-        let imageUrl = URL(string: IMAGE_URL_STR)!
-        
-        DispatchQueue.global().async {
-            let imageData = NSData(contentsOf: imageUrl)!
-            
-            DispatchQueue.main.async {
-                self.imageLoadingIndicator.stopAnimating()
-                
-                let image = UIImage(data: imageData as Data)
-                self.imageView.image = image;
-            }
-        }
-
-        // fetch beverages list data
-        
-        let beveragesUrl = URL(string: BEVERAGES_URL_STR)!
-        
-        var request = URLRequest(url: beveragesUrl)
-        request.httpMethod = "GET"
-        request.setValue("Token \(BEVERAGES_API_TOKEN)", forHTTPHeaderField: "Authorization")
-        
-        let session = URLSession.shared
-        let task = session.dataTask(with: request) {
-            (data, response, error) -> Void in
-
-            let httpResponse = response as! HTTPURLResponse
-            let statusCode = httpResponse.statusCode
-            
-            if (statusCode == 200) {
-                self.parseJson(with:data!)
-            }
-
-            self.beveragesLoadingIndicator.stopAnimating()
-        }
-        
-        beveragesLoadingIndicator.startAnimating()
-        task.resume()
     }
     
     func parseJson(with data: Data) {
-        do {
-            let jsonDict = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? NSDictionary
-            
-            if let results = jsonDict?["result"] as? NSArray {
-                for beverageData in results {
-                    let beverageDictionary = beverageData as? NSDictionary
-                    let name = beverageDictionary?["name"] as! String
-                    let category = beverageDictionary?["primary_category"] as! String
-                    let beverage = Beverage(name: name, category: category)
-                    self.beverages.append(beverage)
-                }
-                
-                self.beveragesTable.reloadData()
-            }
-        } catch {
-            print("JSON parsing failed")
-        }
+        
     }
 }
 
